7fc3b9e80a5c710480ae2ce1d1192e718bea38e5
Ales Rechtorik <foxxcz@gmail.com>
2024-11-02 03:57:35 +0100

    wip

--- supertokens-dashboard/index.mjs
+++ supertokens-dashboard/index.mjs
@@ -1,6 +1,9 @@
 import express from 'express';
 import SuperTokens from 'supertokens-node';
 import Dashboard from 'supertokens-node/recipe/dashboard';
+import Passwordless from 'supertokens-node/recipe/passwordless';
+import Session from "supertokens-node/recipe/session";
+import { SMTPService } from "supertokens-node/recipe/passwordless/emaildelivery";
 import { middleware } from 'supertokens-node/framework/express';
 
 const app = express();
@@ -17,6 +20,23 @@ SuperTokens.init({
   },
   recipeList: [
     Dashboard.init(),
+    Passwordless.init({
+      contactMethod: 'EMAIL',
+      emailDelivery: {
+        service: new SMTPService({
+          smtpSettings: {
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            from: {
+                name: process.env.SMTP_FROM_NAME,
+                email: process.env.SMTP_FROM_EMAIL,
+            },
+            secure: true,
+          },
+        })
+      }
+    }),
+    Session.init(),
   ],
 });
 
